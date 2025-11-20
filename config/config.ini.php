89d51a5e9d7d0c3bbff500c8c416a311a06fa3c6
achuthanprabhukumar-wq <achuthanprabhukumar@gmail.com>
2025-11-20 21:28:30 +0100

    checkin the config changes

--- config/config.ini.php
+++ config/config.ini.php
@@ -1,15 +1,17 @@
 ; <?php exit; ?> DO NOT REMOVE THIS LINE
 
 [database]
-host = "127.0.0.1"
-username = "matomo"
-password = "matomopw"
-dbname = "matomo"
+host = "sql313.infinityfree.com"
+username = "if0_40186077"
+password = "Prabhu2025"
+dbname = "if0_40186077_matomo"
 tables_prefix = "matomo_"
-port = 3307
+port = 3306
 adapter = PDO\MYSQL
 charset = utf8mb4
 
 [General]
 ; Change the salt to a random string for security
 salt = "please-change-this-to-a-random-string"
+; When running in Azure App Service, enable App Service storage mount
+WEBSITES_ENABLE_APP_SERVICE_STORAGE = true
