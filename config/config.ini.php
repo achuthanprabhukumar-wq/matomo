22f40290592d8673cac54e20b8e3b734639f9551
achuthanprabhukumar-wq <achuthanprabhukumar@gmail.com>
2025-11-20 13:10:15 +0100

    Updated auth

---
+++ config/config.ini.php
@@ -0,0 +1,15 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+
+[database]
+host = "127.0.0.1"
+username = "matomo"
+password = "matomopw"
+dbname = "matomo"
+tables_prefix = "matomo_"
+port = 3307
+adapter = PDO\MYSQL
+charset = utf8mb4
+
+[General]
+; Change the salt to a random string for security
+salt = "please-change-this-to-a-random-string"
